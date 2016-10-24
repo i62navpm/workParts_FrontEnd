b95aa04ca97232e6e221b6ef905655fe58cd990b
Manuel Navarro <manuelnavarro1987@gmail.com>
2016-10-24 23:05:06 +0200

    Include router + rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { RouterModule }   from '@angular/router';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { WorkersMock }          from './workers/shared/workers-mock/workers-mock';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { WorkersComponent } from './workers/workers.component';
+
+import './rxjs-extensions';
+import { WorkerComponent } from './workers/worker/worker.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    SidenavComponent
+    SidenavComponent,
+    WorkersComponent,
+    WorkerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    InMemoryWebApiModule.forRoot(WorkersMock),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/workers',
+        pathMatch: 'full'
+      },
+      {
+        path: 'workers',
+        component: WorkersComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
